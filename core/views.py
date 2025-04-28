bdce01fc94c715aa33c36314ccec28d489be0c11
danil-nizamov <danil.nizamov+github.codesignal.com>
2025-04-28 23:52:51 +0300

    added basic functionality

--- core/views.py
+++ core/views.py
@@ -1,3 +1,25 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from .models import Task
 
-# Create your views here.
+
+def task_list(request):
+    if request.method == 'POST':
+        title = request.POST.get('title')
+        if title:
+            Task.objects.create(title=title)
+        return redirect('task_list')
+    tasks = Task.objects.order_by('-created_at')
+    return render(request, 'task_list.html', {'tasks': tasks})
+
+
+def toggle_task(request, task_id):
+    task = get_object_or_404(Task, id=task_id)
+    task.done = not task.done
+    task.save()
+    return redirect('task_list')
+
+
+def delete_task(request, task_id):
+    task = get_object_or_404(Task, id=task_id)
+    task.delete()
+    return redirect('task_list')
